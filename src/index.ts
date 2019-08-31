6b62f6249fbd33f70f4f6e9f831d727168c063a9
John Tucker <jtucker@appirio.com>
2019-08-31 14:57:32 -0400

    wow

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,20 @@
+import 'wc-example';
+
 class HelloUsage extends HTMLElement {
   constructor() {
     super();
     const shadow = this.attachShadow({ mode: 'closed' });
     const rootEl = document.createElement('div');
-    rootEl.textContent = 'Hello Usage';
+
+    // HELLO USAGE
+    const rootHelloUsageEl = document.createElement('div');
+    rootHelloUsageEl.textContent = 'Hello Usage';
+    rootEl.appendChild(rootHelloUsageEl);
+
+    // HELLO WORLD
+    const rootHelloWorldEl = document.createElement('hello-world');
+    rootEl.appendChild(rootHelloWorldEl);
+
     shadow.appendChild(rootEl);
   }
 }
