fe51ec2b66b7445237e277db8013f1c07cf2d666
John Tucker <jtucker@appirio.com>
2019-09-01 13:17:15 -0400

    Done

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,10 @@
 import 'wc-example';
 
 class HelloUsage extends HTMLElement {
+  private rootHelloWorldEl: HTMLElement;
+
+  private value = 0;
+
   constructor() {
     super();
     const shadow = this.attachShadow({ mode: 'closed' });
@@ -12,10 +16,25 @@ class HelloUsage extends HTMLElement {
     rootEl.appendChild(rootHelloUsageEl);
 
     // HELLO WORLD
-    const rootHelloWorldEl = document.createElement('hello-world');
-    rootEl.appendChild(rootHelloWorldEl);
+    this.rootHelloWorldEl = document.createElement('hello-world');
+    this.rootHelloWorldEl.setAttribute('color', 'purple');
+    this.rootHelloWorldEl.setAttribute('value', this.value.toString());
+    rootEl.appendChild(this.rootHelloWorldEl);
 
     shadow.appendChild(rootEl);
   }
+
+  public connectedCallback(): void {
+    this.rootHelloWorldEl.addEventListener('increment', this.handleIncrement);
+  }
+
+  public disconnectedCallback(): void {
+    this.rootHelloWorldEl.removeEventListener('increment', this.handleIncrement);
+  }
+
+  private handleIncrement = (): void => {
+    this.value += 1;
+    this.rootHelloWorldEl.setAttribute('value', this.value.toString());
+  };
 }
 window.customElements.define('hello-usage', HelloUsage);
